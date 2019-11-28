a24f69cca6218fcb849b47e0103761618cf77c93
amrmynd <ariley.amy@gmail.com>
2019-11-28 15:37:20 +0100

    adding components and BookContext

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react';
+import uuid from 'uuid/v1';
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+    const [books, setBooks] = useState([
+        { title: 'IQ84', author: 'haruki murakami'},
+        { title: 'the testaments', author: 'margaret atwood'}
+    ])
+    const addBook = (title, author) => {
+        setBooks([...books, { title, author, id: uuid() }])
+    }
+    const removeBook = (id) => {
+        setBooks(books.filter(book => book.id ==! id))
+    }
+    return (
+        <BookContext.Provider value={{books, addBook, removeBook}}>
+            { props.children }
+        </BookContext.Provider>
+    )
+}
+
+export default BookContextProvider; 
